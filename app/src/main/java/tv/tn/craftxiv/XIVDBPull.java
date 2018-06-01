abc56e09647a97483a96216e2e66df54a4b06d76
Travis <vuong033@umn.edu>
2018-06-01 08:05:49 -0500

    Finished pulling data from three access points: shop, item, recipe.
    Pulls each individual item to their own detail directory for future usage.

--- app/src/main/java/tv/tn/craftxiv/XIVDBPull.java
+++ app/src/main/java/tv/tn/craftxiv/XIVDBPull.java
@@ -1,52 +1,173 @@
 package tv.tn.craftxiv;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Writer;
+
+//URL Connections
 import java.net.URL;
 import java.net.URLConnection;
 
-import org.apache.commons.lang.StringEscapeUtils;
+//File I/O
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.Writer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+//Google/Android implementation JSON
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import org.apache.commons.lang.StringEscapeUtils; //Unicode Support
+
 public class XIVDBPull {
+    private static final int limitAPI = 10;
+
+    private static void queryCollection(String mode){
+        try{
+            File genericFile = new File("app/src/main/res/data/" + mode + ".json");
+            URL xivdbGeneric = new URL("https://api.xivdb.com/" + mode); //URL to parse
+            URLConnection urlGeneric = xivdbGeneric.openConnection();
+            urlGeneric.addRequestProperty("User-Agent","Mozilla/5.0");
+            BufferedReader inputGeneric = new BufferedReader(new InputStreamReader(urlGeneric.getInputStream()));
+
+            Writer genericOut = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(genericFile),"UTF8"));
+            genericOut.append(StringEscapeUtils.unescapeJava(inputGeneric.readLine()));
+            genericOut.flush();
+            genericOut.close();
+            inputGeneric.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void queryDetails(String mode, Object ob){
+        try{
+            JSONObject rec = (JSONObject) ob;
+            String recID = String.valueOf(rec.get("id"));
+            File queryFile = new File("app/src/main/res/data/"+ mode + "/" + recID + ".json");
+            URL detailURL = new URL("https://api.xivdb.com/" + mode + "/" + recID); //URL to parse
+            URLConnection tempDetailURL = detailURL.openConnection();
+            tempDetailURL.addRequestProperty("User-Agent","Mozilla/5.0");
+
+            BufferedReader tempDetailStream = new BufferedReader(new InputStreamReader(tempDetailURL.getInputStream()));
+
+            Writer queryOut = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(queryFile),"UTF8"));
+            queryOut.append(StringEscapeUtils.unescapeJava(tempDetailStream.readLine()));
+            queryOut.flush();
+            queryOut.close();
+            tempDetailStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
+
+        JSONParser parser = new JSONParser();
+
         try{
             //Recipe File
-            File recipeFile = new File("app/src/main/res/data/recipe.json");
-            URL xivdbRecipe = new URL("https://api.xivdb.com/recipe"); //URL to parse
-            URLConnection urlRecipe = xivdbRecipe.openConnection();
-            urlRecipe.addRequestProperty("User-Agent","Mozilla/5.0");
-            BufferedReader inputRecipe = new BufferedReader(new InputStreamReader(urlRecipe.getInputStream()));
+            queryCollection("recipe");
 
-            Writer recipeOut = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(recipeFile),"UTF8"));
-            recipeOut.append(StringEscapeUtils.unescapeJava(inputRecipe.readLine()));
-            recipeOut.flush();
-            recipeOut.close();
-            inputRecipe.close();
 
-            //Item
-            File itemFile = new File("app/src/main/res/data/item.json");
-            URL xivdbItem = new URL("https://api.xivdb.com/item"); //URL to parse
-            URLConnection urlItem = xivdbItem.openConnection();
-            urlItem.addRequestProperty("User-Agent","Mozilla/5.0");
-            BufferedReader inputItem = new BufferedReader(new InputStreamReader(urlItem.getInputStream()));
+            //Item File
+            queryCollection("item");
 
-            Writer itemOut = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(itemFile),"UTF8"));
-            itemOut.append(StringEscapeUtils.unescapeJava(inputItem.readLine()));
-            itemOut.flush();
-            itemOut.close();
-            inputItem.close();
+
+            //Shop File
+            queryCollection("shop");
+
+            int counter = 3;
+
+            //Parse through recipes
+            JSONArray recipeJSON = null;
+            try {
+                recipeJSON = (JSONArray) parser.parse(new FileReader("app/src/main/res/data/recipe.json"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            //Parse through recipe details
+            for (Object o: recipeJSON){
+
+                if(counter >= limitAPI){
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                        queryDetails("recipe", o);
+                        counter = 1;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    JSONObject temp = (JSONObject) o;
+                    String x = String.valueOf(temp.get("id"));
+                    queryDetails("recipe", o);
+                    counter++;
+                }
+            }
+
+
+
+            //Parse through items
+            JSONArray itemJSON = null;
+            try {
+                itemJSON = (JSONArray) parser.parse(new FileReader("app/src/main/res/data/item.json"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            for (Object o: itemJSON){
+
+                if(counter >= limitAPI){
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                        queryDetails("item", o);
+                        counter = 1;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                else{
+                    queryDetails("item", o);
+                    counter++;
+                }
+            }
+
+            //Parse through shops
+            JSONArray shopJSON = null;
+            try {
+                shopJSON = (JSONArray) parser.parse(new FileReader("app/src/main/res/data/shop.json"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            for (Object o: shopJSON){
+
+                if(counter >= limitAPI){
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                        queryDetails("shop", o);
+                        counter = 1;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    queryDetails("shop", o);
+                    counter++;
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
